bee625c58f8c4d307d91f6b2f306feacc339fe86
wfiene <walkerfiene@gmail.com>
2023-03-08 15:13:17 -0500

    table fix

--- app/models/cart.py
+++ app/models/cart.py
@@ -3,8 +3,8 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Cart(db.Model):
     __tablename__ = 'cart'
     
-    if environment == 'production':
-        __table_args__ == {'scheme': SCHEMA}
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
         
     
     id = db.Column(db.Integer, primary_key=True)
